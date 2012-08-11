f63d54c945a3d3a56c6db4c8861b9d1f0c1caa7f
Mike Ivanov <mivanov@gmail.com>
2012-08-10 18:40:14 -0700

    Display as table

--- sapling/infobox/templatetags/infobox_tags.py
+++ sapling/infobox/templatetags/infobox_tags.py
@@ -7,8 +7,14 @@ register = template.Library()
 
 
 @register.simple_tag
-def infobox(entity):
-    return render_to_string('infobox/infobox_snippet.html')
+def infobox(instance):
+    config_cls = instance._eav_config_cls
+    entity = getattr(instance, config_cls.eav_attr)
+    attributes = [{ 'name': a.name,
+                    'value': getattr(entity, a.slug)
+                   } for a in entity.get_all_attributes()]
+    return render_to_string('infobox/infobox_snippet.html',
+                            { 'attributes': attributes })
 
 
 @register.simple_tag(takes_context=True)
@@ -18,8 +24,3 @@ def infobox_form(context, entity):
     rendered = render_to_string('infobox/infobox_form_snippet.html', context)
     return rendered
 
-
-@register.simple_tag
-def infobox_form_media():
-    f = InfoboxForm()
-    return f.media
