de2a044cc164768e69f3bbd7ff5de9239344f976
Mike Ivanov <mivanov@gmail.com>
2012-08-13 16:51:08 -0700

    Prevent saving of page when infobox is saved

--- sapling/infobox/forms.py
+++ sapling/infobox/forms.py
@@ -8,3 +8,8 @@ class InfoboxForm(BaseDynamicEntityForm):
         config_cls = self.instance._eav_config_cls
         self.entity = getattr(self.instance, config_cls.eav_attr)
         self._build_dynamic_fields()
+
+    def save(self, commit=True):
+        # we don't want to save the model instance, just the EAV attributes
+        super(InfoboxForm, self).save(commit=False)
+        self.entity.save()
