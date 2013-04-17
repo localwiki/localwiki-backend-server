7f12be107eb1f442a5b8d248812d9af9715f78c1
Philip Neustrom <philipn@gmail.com>
2013-04-17 04:46:53 -0700

    Use SimpleWikiHTMLField

--- sapling/crap_events/models.py
+++ sapling/crap_events/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from ckeditor.models import HTML5FragmentField
+from pages.fields import WikiHTMLField
+
+
+class SimpleWikiHTMLField(WikiHTMLField):
+    allowed_elements = [
+        'p', 'br', 'a', 'em', 'strong', 'u', 'img', 'h1', 'h2', 'h3',
+        'h4', 'h5', 'h6', 'ul', 'ol', 'li', 'pre', 'span', 'strike',
+        'tt'
+    ]
 
 
 class Event(models.Model):
     title = models.CharField(max_length=250)
     time_start = models.DateTimeField()
     location = models.CharField(max_length=250)
-    description = models.TextField()
+    description = SimpleWikiHTMLField()
     posted_by = models.ForeignKey(User)
     
     class Meta:
