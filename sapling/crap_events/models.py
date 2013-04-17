73904e252eb8353774adf3fd6f0ac85638ab3780
Philip Neustrom <philipn@gmail.com>
2013-04-16 22:05:30 -0700

    Layout list by day + rich text (basics) event desc

--- sapling/crap_events/models.py
+++ sapling/crap_events/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from ckeditor.models import HTML5FragmentField
+
 
 class Event(models.Model):
     title = models.CharField(max_length=250)
@@ -8,3 +10,6 @@ class Event(models.Model):
     location = models.CharField(max_length=250)
     description = models.TextField()
     posted_by = models.ForeignKey(User)
+    
+    class Meta:
+        ordering = ['time_start']
--- sapling/crap_events/views.py
+++ sapling/crap_events/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timedelta
+from itertools import groupby
+
 from django.core.urlresolvers import reverse
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
@@ -5,14 +8,34 @@ from django.views.generic.edit import UpdateView
 from utils.views import CreateObjectMixin, PermissionRequiredMixin
 
 from models import Event
+from forms import EventForm
 
 
 class EventListView(ListView):
     model = Event
 
+    def get_queryset(self):
+        # Only display events that occur in the future, plus ones that
+        # have happened in the past 8 hours.
+        return Event.objects.filter(
+            time_start__gte=(datetime.now() - timedelta(hours=8))
+        )
+
+    def get_context_data(self, *args, **kwargs):
+        def _the_day(e):
+            return (e.time_start.year, e.time_start.month, e.time_start.day)
+
+        context = super(EventListView, self).get_context_data(*args, **kwargs)
+        by_day = [ (datetime(*d), list(events)) for d, events in
+                   groupby(self.get_queryset(), _the_day) ]
+        context['events_by_day'] = by_day
+
+        return context
+
 
 class EventUpdateView(CreateObjectMixin, UpdateView):
     model = Event
+    form_class = EventForm
 
     def get_object(self):
         return Event()
