edeb758adf40e746c6eba6c8eea86adbdeb99732
Philip Neustrom <philipn@gmail.com>
2014-09-14 12:35:24 +0800

    Tour improvements with preloading in iframe

--- localwiki/explore/static/js/random_tour.js
+++ localwiki/explore/static/js/random_tour.js
@@ -2,36 +2,49 @@ var urls = [];
 var tour_i = 0;
 var iterate_tour = true;
 
-function run_tour() {
-    if (!urls) {
-        // At the end, time to reset
+function run_tour(flip) {
+    // First run
+    if (urls.length == 0) {
+        get_urls(flip);
+        return;
     }
-
+    // At the end, time to reset
     if (tour_i >= urls.length) {
-        get_urls();
+        get_urls(flip);
         return;
     }
     if (!iterate_tour) {
         return;
     }
     
-    set_page(urls[tour_i]);
+    set_page(urls[tour_i], flip);
     tour_i++;
 
-    $('#tour').load(function() {
-        setTimeout(run_tour, 4500);
+    $('#tour' + flip).unbind('load');
+    $('#tour' + flip).load(function() {
+        flip = (flip + 1) % 2;
+        setTimeout(function(){ run_tour(flip); }, 1500);
     });
 }
 
-function get_urls() {
-    $.getJSON("rtour_json", function(data) {
+function get_urls(flip) {
+    var get_url = "/_explore/rtour_json/";
+    if (typeof(region_slug) != 'undefined' && region_slug) {
+        get_url = "/" + region_slug + get_url;
+    }
+    $.getJSON(get_url, function(data) {
         urls = data['urls'];
-        run_tour();
+        if (urls.length > 0) {
+            run_tour(flip);
+        }
     });
 }
 
-function set_page(url) {
-    $('#tour').attr('src', url);
+function set_page(url, flip) {
+    $('#tour' + flip).attr('src', url);
+    $('#tour' + flip);
+    flip = (flip + 1) % 2
+    $('#tour' + flip).show();
 }
 
 $(document).ready(function() {
@@ -45,7 +58,7 @@ $(document).ready(function() {
         iterate_tour = true;
         $('#stop').show();
         $('#start').hide();
-        run_tour();
+        run_tour(0);
     });
-    run_tour();
+    run_tour(0);
 });
