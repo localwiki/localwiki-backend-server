ec1dec03b6f01472f4be39c47c6a4ca3e538100b
Philip Neustrom <philipn@gmail.com>
2014-10-19 22:57:46 +0700

    Switch default fab branch for travis tests

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -204,7 +204,7 @@ env.localwiki_root = '/srv/localwiki'
 env.src_root = os.path.join(env.localwiki_root, 'src')
 env.virtualenv = os.path.join(env.localwiki_root, 'env')
 env.data_root = os.path.join(env.virtualenv, 'share', 'localwiki')
-env.branch = 'hub'
+env.branch = 'interim_splash_site'
 env.git_hash = None
 
 def production():
