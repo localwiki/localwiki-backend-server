ee87bcdeea7c08e4da84a2632d779cc8126938a7
Philip Neustrom <philipn@gmail.com>
2015-01-26 15:12:55 +0800

    Update es translation.

--- localwiki/blog/urls.py
+++ localwiki/blog/urls.py
@@ -7,20 +7,34 @@ from django.conf import settings
 from .feeds import BlogPostsFeed
 from .models import Post
 
-qs = Post.objects.all()
-
 class FullBodyFeed(BlogPostsFeed):
     def item_description(self, item):
         return item.body
 
 
+class BlogListView(ArchiveIndexView):
+    def get_queryset(self, *args, **kwargs):
+        if self.request.user.is_superuser:
+            return Post.objects.all()
+        else:
+            return Post.objects.published()
+
+
+class BlogPageView(DetailView):
+    def get_queryset(self, *args, **kwargs):
+        if self.request.user.is_superuser:
+            return Post.objects.all()
+        else:
+            return Post.objects.published()
+
+
 urlpatterns = patterns('views',
     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$',
-        DetailView.as_view(queryset=qs),
+        BlogPageView.as_view(),
         name='blog_detail'
     ),
     url(r'^$',
-        ArchiveIndexView.as_view(queryset=qs, paginate_by=settings.BLOG_PAGESIZE, date_field='publish', allow_future=False),
+        BlogListView.as_view(paginate_by=settings.BLOG_PAGESIZE, date_field='publish', allow_future=False),
         name='blog_index',
     ),
     (r'^feed/$', FullBodyFeed()),
