8b501e0067f73678cb61226fa824f62cba8cf800
Philip Neustrom <philipn@gmail.com>
2015-02-23 12:36:02 -0800

    Fixes #63.

--- localwiki/tags/urls.py
+++ localwiki/tags/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import *
 from tags.views import TagListView, TaggedList, GlobalTaggedList, AddSingleTagView
 
 urlpatterns = patterns('',
-    url(r'^(?P<region>[^/]+?)/tags/$', TagListView.as_view(), name='list'),
-    url(r'^(?P<region>[^/]+?)/tags/(?P<slug>.+)/*$', TaggedList.as_view(), name='tagged'),
+    url(r'^(?P<region>[^/]+?)/((?i)tags)/$', TagListView.as_view(), name='list'),
+    url(r'^(?P<region>[^/]+?)/((?i)tags)/(?P<slug>.+)/*$', TaggedList.as_view(), name='tagged'),
     url(r'^(?P<region>[^/]+?)/_add_tag/$', AddSingleTagView.as_view(), name='add-single'),
 
     url(r'^tags/(?P<slug>.+)/*$', GlobalTaggedList.as_view(), name='global-tagged'),
